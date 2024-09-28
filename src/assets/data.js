abd9da429a673430295e7996c73d8220501abcf6
onlyonealexia <alwxiachukwuma@gmial.com>
2024-09-28 13:15:26 +0100

    main

---
+++ src/assets/data.js
@@ -0,0 +1,44 @@
+//array containing objects for my questions,,option and awnsers
+
+export const data =[ {
+    question: "Which device is used for internet connection",
+    option1: "Modem",
+    option2: "Router",
+    option3: "LAN Cable",
+    option4: "Pen Drive",
+    ans: 1,
+},
+
+{ question: "Spell the name Alexia",
+option1: "Medusa",
+option2: "Jenny",
+option3: "Luna",
+option4: "Alexia",
+ans: 4,},
+
+{ question: "Where is web3bride located",
+option1: "Ikorodu",
+option2: "London",
+option3: "Africa",
+option4: "Heaven",
+ans: 4,},
+
+{ question: "who is the founder of web3bridge",
+option1: "Mr beans",
+option2: "Mr ayo",
+option3: "Mr Abims",
+option4: "MIss Alexia",
+ans: 2,},
+
+{ question: "Is the moon lovable",
+option1: "Definitly",
+option2: "Probably",
+option3: "Nop",
+option4: "Most Defenitly",
+ans: 4,},
+
+
+
+
+
+]
